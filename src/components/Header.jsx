070abfb6bdd746e617249532b715e859913759a4
subhdas1993 <subhashis02rgt2017@gmail.com>
2026-06-03 18:09:12 +0530

    Add Some More in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,31 @@ import React from 'react'
 import logo from '../assets/images/logo.png'
 import phone from '../assets/images/contactimg.png'
 
-function Header({headerRef}) {
+function Header({ headerRef }) {
   return (
-    <div className='headerSection' ref={headerRef}>
-      <div className='logoSection'>
-        <img src={logo} alt="Logo" className="logo" />
-      </div>
-      <div className='contactSection'>
-        <button className="contactBtn">
-            <span className="icon">
-              <img src={phone} alt="Phone" />
-            </span>
+    <>
+      <div className='headerSection' ref={headerRef}>
+        <div>
+          <div className='logoSection'>
+            <img src={logo} alt="Logo" className="logo" />
+          </div>
+          <div className='contactSection'>
+            <button className="contactBtn">
+              <span className="icon">
+                <img src={phone} alt="Phone" />
+              </span>
 
-            <span className="phoneNumber">
-              01169328350
-            </span>
-        </button>
+              <span className="phoneNumber">
+                01169328350
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          Starting at <span>Rs 69,999</span> Rs 47,999. Hurry! Offer ends soon.
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
